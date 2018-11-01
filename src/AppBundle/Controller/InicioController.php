c77930e6ed8a32644c3681017f85e36c7d357a1f
JJ <mtbxpress@gmail.com>
2018-11-01 13:13:09 +0100

    Commit

---
+++ src/AppBundle/Controller/InicioController.php
@@ -0,0 +1,22 @@
+<?php
+/*
+ * Controlador Inicio, Pagina principal
+ * @version 0.1
+ * @author Juan José Delgado Romero
+ */
+
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class InicioController extends Controller{
+
+    public function inicioAction(Request $request)
+    {
+        return $this->render('Inicio/inicio.html.twig');
+       // return $this->render('@App/base_menu.html.twig');
+    }
+}
